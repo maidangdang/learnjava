f9569dd1724a5bc0870dd0c95d7a81f084d9156f
maiyihua <258113781@qq.com>
2018-07-09 22:15:05 +0800

    first commit

---
+++ src/main/java/maidangdang/learnjava/test/Test.java
@@ -0,0 +1,10 @@
+package maidangdang.learnjava.test;
+
+/**
+ * Created by mai on 2018/7/1.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
